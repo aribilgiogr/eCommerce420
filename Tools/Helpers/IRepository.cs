23efd154fab063a453c8c2cddf928babfc1525d2
aribilgiogr <aribilgiogr@gmail.com>
2026-05-11 20:49:56 +0300

    Add project files.

---
+++ Tools/Helpers/IRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tools.Helpers
+{
+    public interface IRepository<T> where T : class
+    {
+        Task CreateAsync(T entity);
+        Task CreateManyAsync(IEnumerable<T> entities);
+
+        Task<T?> ReadAsync(object key);
+        Task<IEnumerable<T>> ReadManyAsync(Expression<Func<T, bool>>? expression = null, params string[] includes);
+
+        Task UpdateAsync(T entity);
+        Task UpdateMany(IEnumerable<T> entities);
+
+        Task DeleteAsync(object key);
+        Task DeleteAsync(T entity);
+        Task DeleteManyAsync(IEnumerable<T> entities);
+        Task DeleteManyAsync(Expression<Func<T, bool>>? expression = null);
+
+        Task<int> CountAsync(Expression<Func<T, bool>>? expression = null);
+        Task<bool> AnyAsync(Expression<Func<T, bool>>? expression = null);
+    }
+}
