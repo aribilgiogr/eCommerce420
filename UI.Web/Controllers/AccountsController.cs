55359dd0f549608deb37a133f2e338358592cbe7
aribilgiogr <aribilgiogr@gmail.com>
2026-05-15 20:58:02 +0300

    15.05.2026 Update 02

---
+++ UI.Web/Controllers/AccountsController.cs
@@ -0,0 +1,43 @@
+﻿using Core.Concretes.DTOs;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UI.Web.Controllers
+{
+    public class AccountsController : Controller
+    {
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login(string? returnUrl)
+        {
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Login(LoginDto model, string? returnUrl)
+        {
+            return View();
+        }
+
+        public IActionResult Register(string? returnUrl)
+        {
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Register(RegisterDto model, string? returnUrl)
+        {
+            return View();
+        }
+
+        [HttpPost, Authorize]
+        public IActionResult Logout(string? returnUrl)
+        {
+            return Redirect(returnUrl ?? "/");
+        }
+    }
+}
